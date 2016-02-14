f246719a3ec8e4e1582a05471ca017dc0197e493
jc3213 <jc3213@users.noreply.github.com>
2016-02-14 16:40:29 +0800

    Create pref-utils.js

---
+++ SimpleProxy/lib/pref-utils.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var SimplePrefs = require("sdk/simple-prefs");
+
+exports.getValue = function (name) {
+  return SimplePrefs.prefs[name];
+};
+exports.setValue = function (name, value) {
+  SimplePrefs.prefs[name] = value;
+};
+exports.addListener = function (name, callback) {
+  SimplePrefs.on(name, callback);
+};
+exports.removeListener = function (name, callback) {
+  SimplePrefs.off(name, callback);
+};
