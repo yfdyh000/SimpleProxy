b85e4df69feca39f82506df64e711101f62ba626
jc3213 <jc3213@users.noreply.github.com>
2016-02-14 16:40:49 +0800

    Create storage.js

---
+++ SimpleProxy/lib/storage.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.profile0 = {
+  list: new Object(),
+  server: new Object()
+};
+exports.profile1 = {
+  list: new Object(),
+  server: new Object()
+};
+exports.profile2 = {
+  list: new Object(),
+  server: new Object()
+};
