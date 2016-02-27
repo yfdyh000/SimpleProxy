f453a8c4b330117c169b2839f09d742811da4111
jc3213 <jc3213@users.noreply.github.com>
2016-02-27 18:28:24 +0800

    Update proxy.js

--- SimpleProxy/data/proxy.js
+++ SimpleProxy/data/proxy.js
@@ -3,27 +3,27 @@
 var Storage = require("../lib/storage.js");
 var Services = require("../lib/services.js");
 
-var white, match;
+var white, match, server;
 
 var Proxy = {
   applyFilter: function (service, uri, proxy) {
     for (var i in Storage) {
-      if (!Storage[i].server.property || !Storage[i].list.file) continue;
+      if (!(server = Storage[i].server.property) || !Storage[i].list.file) continue;
 
       if (white = Storage[i].list.pattern.white) {
-        white.forEach(function (element, index, array) {
-          if (element.matches(uri)) {
+        for (var i in white) {
+          if (white[i].matches(uri)) {
             return proxy;
           }
-        });
+        }
       }
 
       if (match = Storage[i].list.pattern.match) {
-        match.forEach(function (element, index, array) {
-          if (element.matches(uri)) {
+        for (var x in match) {
+          if (match[x].matches(uri)) {
             return server;
           }
-        });
+        }
       }
     }
     return proxy;
