02150c6e0cc968e76accc20b422be998f51b2b39
jc3213 <jc3213@users.noreply.github.com>
2016-03-20 09:48:52 +0800

    Update proxy.js

--- SimpleProxy/data/proxy.js
+++ SimpleProxy/data/proxy.js
@@ -3,30 +3,48 @@
 var Storage = require("../lib/storage.js");
 var Services = require("../lib/services.js");
 
-var white, match, server;
+var server;
 
 var Proxy = {
   applyFilter: function (service, uri, proxy) {
     for (var i in Storage) {
       if (!(server = Storage[i].server.property) || !Storage[i].list.file) continue;
 
-      var patterns = Storage[i].list.pattern
-      if (white = patterns.white) {
-        for (var i in white) {
-          if (white[i].matches(uri)) {
+      var white = Storage[i].list.white;
+      var match = Storage[i].list.match;
+
+      for (var x in white) {
+        if (white[x].length == 0) continue;
+
+        for (var r in white[x]) {
+          var _rule = white[x][r];
+
+          if (Proxy.isMatched(x, _rule, uri)) {
             return proxy;
           }
         }
       }
-      if (match = patterns.match) {
-        for (var x in match) {
-          if (match[x].matches(uri)) {
+
+      for (var y in match) {
+        if (match[y].length == 0) continue;
+
+        for (var s in match[y]) {
+          var rule = match[y][s];
+
+          if (Proxy.isMatched(y, rule, uri)) {
             return server;
           }
         }
       }
     }
     return proxy;
+  },
+  isMatched: function (method, rule, uri) {
+    if (method == 'regexp' && rule.test(uri.spec)) {
+      return true;
+    } else if (method == 'string' && uri.spec.includes(rule)) {
+      return true;
+    }
   }
 }
 
